79de0aa6949b635aa99a99b2a95e43d31142f4a4
Ki-M4N <franprocaset@gmail.com>
2026-02-12 12:31:01 +0100

    Update discord_bot.sh

--- discord_bot.sh
+++ discord_bot.sh
@@ -21,5 +21,5 @@ fi
 echo ""
 echo "🚀 Starting Discord Bot..."
 export MODE=discord
-export DISCORD_TOKEN="MTQ3MDQ0OTg4Mzk1MjcwOTcxNA.GELb3A.85d6D4V3UO9b7Wa8yqRuKnFkvLrustjmcnNORg"
+export DISCORD_TOKEN=""
 python3 discord_integration.py
