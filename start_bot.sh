9cdad507429a9297ebc9ba9a61d6d14bf03e9418
Ki-M4N <franprocaset@gmail.com>
2026-02-12 12:32:56 +0100

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -14,7 +14,7 @@ case $choice in
     1)
         echo "🚀 Starting Discord Bot..."
         export MODE=discord
-        export DISCORD_TOKEN="MTQ3MDQ0OTg4Mzk1MjcwOTcxNA.GELb3A.85d6D4V3UO9b7Wa8yqRuKnFkvLrustjmcnNORg"
+        export DISCORD_TOKEN="YOUR DISCORD TOKEN"
         python3 discord_integration.py
         ;;
     2)
